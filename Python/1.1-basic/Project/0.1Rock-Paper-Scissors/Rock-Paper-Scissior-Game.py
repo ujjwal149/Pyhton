221640741ddd4b63aa36cae5c4097e5a55ae8b8f
ujjwal <ujjwal149209@gmail.com>
2026-01-15 22:04:50 +0530

    remove bugs in logic

--- Python/1.1-basic/Project/0.1Rock-Paper-Scissors/Rock-Paper-Scissior-Game.py
+++ Python/1.1-basic/Project/0.1Rock-Paper-Scissors/Rock-Paper-Scissior-Game.py
@@ -26,13 +26,13 @@ scissors ='''
 ---.__(___)
 '''
 
-user_input = input("Enter 1 to select  rock , 2 to select paper  and 2 to select scissors: ")
+user_input = input("Enter 1 to select  rock , 2 to select paper  and 3 to select scissors: ")
 print("You chose: ", user_input)
 
 computer_input = random.randint(1,3)
 if computer_input == 1:
-    print(f"Computer chose ${print(rock)}")
+    print(f"Computer chose {print(rock)}")
 elif computer_input == 2:
-    print(f"Paper chose ${print(paper)}")
+    print(f"Paper chose {print(paper)}")
 elif computer_input == 3:
-    print(f"Rock chose ${print(scissors)}")
+    print(f"Rock chose {print(scissors)}")
