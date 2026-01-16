07f0c33276116d57d72f0432e85099de183b1d9d
ujjwal <ujjwal149209@gmail.com>
2026-01-16 15:34:55 +0530

    Added dictionaries in dataStructure

--- Python/1.1-basic/DataStructure.py
+++ Python/1.1-basic/DataStructure.py
@@ -1,6 +1,7 @@
-
 import random
-
+'''Python provides versatile data structures
+ like lists, tuples, and dictionaries to store and manipulate data efficiently.
+  Below are examples and explanations for each:'''
 
 friends_list = ["Alice","Bob","Charlie","David"]
 
@@ -16,3 +17,27 @@ vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
 dirty_dozen = [fruits, vegetables]
 
 print(dirty_dozen[1][1])
+
+
+
+#Initilize Dictionaries
+
+# store data in key:value pair by the help of key we can access value.
+student_score = {'Harry':29,'Jack':30,'Jordan':21,'Adwin':28,}
+print(student_score['Jordan'])
+
+#we can pass new data inside dictionaries
+student_score['Natasha']=30
+print(student_score)
+#we can manipulate data inside dictionaries
+student_score['Harry']=30
+print(student_score)
+#we can remove all data from dictionaries
+# student_score = {}
+# print(student_score)
+
+#loop inside dictionaries
+for i in student_score:
+    print(i)
+    print(student_score[i])
+
