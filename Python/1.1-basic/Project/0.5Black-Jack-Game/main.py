8a14bdb66e7ce21f8966cf03eb25c74bca3df91b
ujjwal <ujjwal14920@gmail.com>
2026-01-24 18:15:55 +0530

    new back jack game project initiated

---
+++ Python/1.1-basic/Project/0.5Black-Jack-Game/main.py
@@ -0,0 +1,65 @@
+import random
+from logo  import logo
+
+
+def deal_card():
+    cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+    card = random.choice(cards)
+    return card
+
+
+def calculate_score(cards):
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
+
+def compare(user_score , computer_score):
+    if user_score == computer_score:
+        return "Draw 🙄"
+    elif computer_score == 0:
+        return "Lose,Opponent has BlackJack 😱"
+    elif user_score == 0:
+        return "Win with a BlackJack 😎"
+    elif user_score > 21:
+        return "You went over,You lose 😭"
+    elif computer_score > 21:
+        return "computer went over You win 😁"
+    elif user_score > computer_score:
+        return "You win 😀"
+    else:
+        return "You lose "
+
+
+
+user_cards = []
+computer_cards = []
+computer_score = -1
+user_score = -1
+is_game_over = False
+
+for _ in range(2):
+    user_cards.append(deal_card())
+    computer_cards.append(deal_card())
+
+while not is_game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"Your cards: {user_cards}  You score:{user_score}")
+    print(f"Computer's card: {computer_cards[0]} ")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to get another card , type 'n' to pass: ")
+        if user_should_deal == "y":
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
+
+while computer_score != 0 and computer_score < 17 :
+    computer_cards.append(deal_card())
+    computer_score = calculate_score(computer_cards)
