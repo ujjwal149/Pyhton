108fd6e71e8e081e2fe46fd7c153856723748d4d
ujjwal <ujjwal14920@gmail.com>
2026-04-26 14:42:15 +0530

    sun rise and sun set time using API

--- Python/API/main.py
+++ Python/API/main.py
@@ -1,7 +1,27 @@
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+
+#GET INTERNATIONAL SPACE STATION LOCATION.
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json() ["iss_position"]
+# print(data)
+
+
+#GET SUN RISE AND SUN SET TIME OF ANY PARTICULAR LATITUDE AND LONGITUDE.
+parameters = {
+    "lat":30.744600,
+    "lng":76.652496,
+    "formatted":0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json",parameters)
 response.raise_for_status()
 
-data = response.json() ["iss_position"]
-print(data)
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
+print(f"Sun rise at: {sunrise}")
+print(f"Sun set at: {sunset}")
